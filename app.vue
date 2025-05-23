ffd5519ac9533ea71cee158266bfd459fd665cae
Michaela Havlíková <Michelle.h@seznam.cz>
2025-05-23 19:45:03 +0200

    create landing page

--- app.vue
+++ app.vue
@@ -1,10 +1,5 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline text-red-500">
-      Hello world!
-    </h1>
-    <button class="btn btn-secondary">
-      View map <Icon name="tabler:map" />
-    </button>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ components/app/nav-bar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="navbar bg-primary text-primary-content">
+    <div class="navbar-start">
+      <NuxtLink to="/" class="btn btn-ghost text-xl">
+        Nomad Log
+      </NuxtLink>
+    </div>
+    <div class="navbar-end">
+      <a class="btn btn-accent">
+        Sign In with GitHub
+        <Icon name="tabler:brand-github" size="28" />
+      </a>
+    </div>
+  </div>
+</template>
